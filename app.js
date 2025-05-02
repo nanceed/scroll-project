d04eb957bec872fcff97fd60dfbcebf089f29b95
Nancy <duong.nancy@gmail.com>
2025-05-01 21:25:08 -0700

    Added functionality for fixed navbar

--- app.js
+++ app.js
@@ -27,8 +27,27 @@ if (containerHeight === 0) {
     }
 });
 
-// ************ fixed navbar **********
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
 
+// ************ fixed navbar **********
+window.addEventListener('scroll', function () {
+    //console.log(window.pageYOffset);
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add("fixed-nav");
+    } else {
+        navbar.classList.remove("fixed-nav");
+    }
+    
+    if (scrollHeight > 500) {
+        topLink.classList.add("show-link");
+    } else {
+        topLink.classList.remove("show-link");
+    }
+
+});
 // ************ smooth scroll **********
 
 // select links 
