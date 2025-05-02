1b608f1f2d1ec0f4ac7369d0179128096c3f8cd3
Nancy <duong.nancy@gmail.com>
2025-05-01 18:09:50 -0700

    Added navbar toggle functionality

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
 // ************ close links ***********
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener('click', function () {
+    //linksContainer.classList.toggle("show-links");
+    //can use this if you're not using any dynamic data
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    //console.log(containerHeight);
+    const linksHeight = links.getBoundingClientRect().height;
+    console.log(linksHeight);
+
+if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+} else {
+    linksContainer.style.height = 0;
+    }
+});
 
 // ************ fixed navbar **********
 
